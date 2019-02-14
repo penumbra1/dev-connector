209109a33842cca607cf25248796f8bb1c61ef29
Olga Isakova <penumbral@yandex.ru>
2019-02-15 03:25:23 +0500

    User presave, bcrypt, gravatar

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,19 +1,48 @@
-import { prop, Typegoose, ModelType, InstanceType } from "typegoose";
+import { prop, pre, Typegoose } from "typegoose";
+import bcrypt from "bcryptjs";
+import gravatar from "gravatar";
 
+const SALT_ROUNDS = 10;
+
+@pre<User>("save", async function(next) {
+  if (!this.avatar || this.isModified("email")) {
+    // Gravatar params: {size, rating, default}, true for https
+    this.avatar = gravatar.url(
+      this.email,
+      { s: "200", r: "pg", d: "retro" },
+      true
+    );
+  }
+  if (this.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(SALT_ROUNDS);
+      this.password = await bcrypt.hash(this.password, salt);
+    } catch (err) {
+      return next(err);
+    }
+  }
+  return next();
+})
 class User extends Typegoose {
   @prop({ required: true })
-  name: String;
+  name: string;
 
   @prop({ required: true })
-  email: String;
+  email: string;
 
   @prop({ required: true })
-  password: String;
+  password: string;
 
-  @prop({ required: true }) // Either a provided image or a placeholder
-  avatar: String;
+  @prop({
+    // default: () => {
+    //   console.log(this);
+    //   gravatar.url(this.email, { s: "200", r: "pg", d: "retro" }, true);
+    // }
+    required: true
+  })
+  avatar: string;
 
-  @prop({ default: Date.now() })
+  @prop({ default: Date.now })
   date: Date;
 }
 
--- server/routes/api/users.ts
+++ server/routes/api/users.ts
@@ -1,9 +1,34 @@
 import express from "express";
+import UserModel from "../../models/User";
+
+const router = express.Router();
 
 // @route  GET api/users/test
 // @desc Tests the users route
 // @access Public
-const router = express.Router();
 router.get("/test", (req, res) => res.json({ msg: "Users route works" }));
+
+// @route  GET api/users/register
+// @desc Regsiters the user
+// @access Public
+router.post("/register", (req, res, next) => {
+  UserModel.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      // 409 Conflict
+      // Note: user enumeration vulnerability
+      // https://stackoverflow.com/questions/9269040
+      return res.status(409).json({ message: "E-mail already registered" });
+    } else {
+      const { name, email, password } = req.body;
+      const newUser = new UserModel({ name, email, password });
+      newUser
+        .save()
+        .then(({ name, email, avatar }) =>
+          res.status(200).json({ name, email, avatar })
+        )
+        .catch(e => next(e));
+    }
+  });
+});
 
 export default router;
